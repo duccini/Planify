96eaafb57daa74ff00152c924298f790de92a826
duccini <guilhermeduccini@gmail.com>
2025-01-16 16:15:44 -0300

    config: create project with react native cli

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
